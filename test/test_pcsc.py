e4e48b0269029a504588b01c319d1d6395661de6
gvigroux <gvigroux@fee.fr>
2025-04-04 15:46:26 +0200

    v0.0.5 - Fix

--- test/test_pcsc.py
+++
@@ -1,7 +0,0 @@
-from thalessecuritykey.pcsc import PcscThalesDevice
-from thalessecuritykey.device import ThalesDevice
-from unittest import mock
-
-def test_pcsc_call_cbor():
-    device = mock.Mock()
-    PcscThalesDevice(device, "Mock")
